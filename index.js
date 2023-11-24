67e06e25dd5aff739ecd29cd2d116e771e78aae6
Valerio Versace <mail@valerioversace.it>
2023-11-24 08:07:20 +0100

    Change API message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http")
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" })
   res.end(JSON.stringify({
-    "message": "Hello World!"
+    "message": "Hello UniMoRE!"
   }))
 })
 
